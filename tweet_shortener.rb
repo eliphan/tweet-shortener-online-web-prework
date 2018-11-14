e3fd888a427f6959173eda4cf9483b46164b8971
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-14 21:30:54 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,8 +40,8 @@ def selective_tweet_shortener(tweet)
 end
   
 def shortened_tweet_truncator(tweet)
-  if tweet.length > 140
-    word_substituter(tweet)
+  if word_substituter(tweet).length > 140
+     word_substituter(tweet)
    puts tweet[0..140]
  else
    tweet
