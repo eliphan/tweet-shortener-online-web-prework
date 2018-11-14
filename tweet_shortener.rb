137d200fc3d1789c20d66e85ba15ea5e2ee75f63
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-14 21:37:00 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -47,3 +47,18 @@ def shortened_tweet_truncator(tweet)
   end
  end
  
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0..136] + '...'
+  else
+    tweet
+  end
+end
